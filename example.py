5caf936dfdfdfcf5fc7370f9bbb18bc6c3e1d684
CNECCC-CSY <s2023022@gm.cneccc.edu.hk>
2025-06-02 21:42:44 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,9 @@
+# test if ur Nah works!
+return(Nah) # it should print false
+
+# lets try some examples here!
+If Nah:
+  print("Yep") # not going to happen because Nah is False
+else:
+  print("Nuh uh") #will happen because "Fasle else" is true
+
